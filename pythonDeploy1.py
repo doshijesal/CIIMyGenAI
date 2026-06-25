6de22892d7cd12e7a4d4be1070005b2a44d3d329
doshijesal <167989468+doshijesal@users.noreply.github.com>
2026-06-25 18:39:50 +0530

    Update pythonDeploy1.py

--- pythonDeploy1.py
+++ pythonDeploy1.py
@@ -2,20 +2,21 @@ import streamlit as st
 from google import genai
 st.markdown(
   """
-  <h1 style='text-align: center;'> Python AI Assistant</h1>
+  <h1 style='text-align: center;'> Your AI Assistant</h1>
   <p style='text-align: center; font-size:18px;'>
-    Ask any Python programming question.
+    How can I help you. Ask anything you like.
   </p>
   """,
   unsafe_allow_html=True,
 )
 #robo = genai.Client(api_key="MY_APIKEY")
 robo = genai.Client(api_key=st.secrets["MY_APIKEY"])
-mychat = robo.chats.create(model="gemini-flash-lite-latest")
+#mychat = robo.chats.create(model="gemini-flash-lite-latest")
+mychat = robo.chats.create(model="gemini-3.1-flash-lite")
 #Placeholder for the response
 response_placeholder = st.empty()
 
-question = st.text_input("", placeholder="Enter your Python question here...")
+question = st.text_input("", placeholder="Enter your question here...")
 
 col1, col2, col3 = st.columns([4, 1, 4])
 
