2c59e79c3822f7d9a02c9f626d4a93a586931829
doshijesal <167989468+doshijesal@users.noreply.github.com>
2026-06-26 14:51:07 +0530

    Update pythonDeploy1.py

--- pythonDeploy1.py
+++ pythonDeploy1.py
@@ -9,7 +9,7 @@ st.markdown(
   """,
   unsafe_allow_html=True,
 )
-#robo = genai.Client(api_key="MY_APIKEY")
+#robo = genai.Client(api_key="MY_API_KEY")
 robo = genai.Client(api_key=st.secrets["MY_APIKEY"])
 mychat = robo.chats.create(model="gemini-flash-lite-latest")
 #mychat = robo.chats.create(model="gemini-3.1-flash-lite")
